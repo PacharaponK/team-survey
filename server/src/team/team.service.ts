69a64950f1d39c285d10796ef6c890db7c348a58
wildan phidrai-ngam <wildan8376@gmail.com>
2024-11-12 13:09:06 +0700

    update team service

--- server/src/team/team.service.ts
+++ server/src/team/team.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Team } from './entities/team.entity';
 import { Repository } from 'typeorm';
 import { CreateTeamDto } from './dto/create-team.dto';
+import { UpdateTeamDto } from './dto/update-team.dto';
 
 
 @Injectable()
@@ -24,6 +25,40 @@ export class TeamsService {
     } catch (error) {
       throw new Error('Failed to create team: ' + error.message);
     }
+  }
+
+  async updateTeam(id: number, payload: any) {
+    try {
+      const team = await this.teamsRepository.findOne({ where: { id }, relations: ['leader', 'members'] })
+
+      // return team;
+
+      if (!team) {
+        throw new Error(`Team with ID ${id} not found`);
+      }
+
+      if (payload.name) {
+        team.name = payload.name;
+      }
+
+      if (payload.leader) {
+        team.leader = payload.leader;
+      }
+
+      if (payload.members) {
+        team.members = Array.from(new Set([...team.members.map(user => ({ id: user.id })), ...payload.members.map(id => ({ id: id }))]));
+      }
+
+      return await this.teamsRepository.save(team);
+    }
+
+    catch (error) {
+      throw new Error('Failed to create team: ' + error.message);
+    }
+  }
+
+  async getTeamById(id: number) {
+    return await this.teamsRepository.findOne({ where: { id: id }, relations: ['leader', 'members'] })
   }
 
 }
