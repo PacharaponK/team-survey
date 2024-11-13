65016c917f88eab9a5d5658e2df83706830f5ac6
พชรพล เกตุแก้ว <ballxlenver7@gmail.com>
2024-11-13 16:41:52 +0700

    urlPrefix config for emulator or your device

--- client/conf/main.js
+++ client/conf/main.js
@@ -1,5 +1,6 @@
 const conf = {
-	urlPrefix: "http://10.0.2.2:3000",
+	urlPrefix: "http://10.0.2.2:3000", // กรณีรัน client บน Andriod Emulator
+	urlPrefixGlobal: "http://ไอพีของเครื่องตัวเอง:3000", // กรณีรัน client บน Web หรือ Device ของตัวเอง
 	loginEndpoint: "/auth/login",
 	registerEndpoint: "/auth/local/register",
 	jwtUserEndpoint: "/users/profile",
