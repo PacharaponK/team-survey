809df12ebe34b8ae3cd975fbaf84a05520f95ac3
พชรพล เกตุแก้ว <ballxlenver7@gmail.com>
2024-11-18 14:03:37 +0700

    update SDK version and all dependencies

--- client/constants/mocks.js
+++ client/constants/mocks.js
@@ -6,7 +6,7 @@ const user = {
     firstname: "ครูบา",
     lastname: "ช่วยหมูเด้ง",
     phone_number: "0812930490",
-    profileURL: "https://scontent.furt1-1.fna.fbcdn.net/v/t39.30808-6/330966750_3894652390790715_7661189408028264880_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=bIRJU-yIM9cQ7kNvgF6Ctwa&_nc_zt=23&_nc_ht=scontent.furt1-1.fna&_nc_gid=AbCozWIreRSt6lgBdX0EoO_&oh=00_AYA86vl9q_G__b_Wf_cs5_1F7ZcvJQemLjYQRNg9Bb89tA&oe=67353A7D",
+    profileURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHMJzlOrGqRISCrRjUAhBxscV351LczVbMMg&s",
     role: {
         id: 1,
         name: "Admin",  
