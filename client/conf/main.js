944d3bc13e5a40c95d8505386dbf986df2869eb8
พชรพล เกตุแก้ว <ballxlenver7@gmail.com>
2024-11-18 14:05:06 +0700

    change urlPrefix

--- client/conf/main.js
+++ client/conf/main.js
@@ -1,5 +1,5 @@
 const conf = {
-	urlPrefix: "http://172.20.10.2:3000", // กรณีรัน client บน Andriod Emulator
+	urlPrefix: "http://10.0.2.2:3000", // กรณีรัน client บน Andriod Emulator
 	urlPrefixGlobal: "http://ไอพีของเครื่องตัวเอง:3000", // กรณีรัน client บน Web หรือ Device ของตัวเอง
 	loginEndpoint: "/auth/login",
 	registerEndpoint: "/auth/local/register",
